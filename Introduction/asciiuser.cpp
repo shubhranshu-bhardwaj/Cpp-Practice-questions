69796de91dde55f1b3516ed0f81239d0b09153bf
Shubhranshu Bhardwaj <94945909+shubhranshu-bhardwaj@users.noreply.github.com>
2022-07-06 15:51:11 +0530

    Find ASCII Value of a Character take input from user
    C++ Program to Find ASCII Value of a Character take input from user

--- Introduction/asciiuser.cpp
+++ Introduction/asciiuser.cpp
@@ -1,13 +1,13 @@
 //C++ Program to Find ASCII Value of a Character take input from user
 
-#include<iostream>
-using namespace std;
-int main(){
-    char x,y;
-    cout<<"Enter character in lowercase to find the ASCII value:";
-    cin>>x;
+#include<iostream> //header file
+using namespace std; //namespace
+int main(){ //main function
+    char x,y; //declaring variable
+    cout<<"Enter character in lowercase to find the ASCII value:"; //simple printing message on screen
+    cin>>x; //taking input
     cout<<"Enter the same character in uppercase:";
     cin>>y;
-    cout<<"ASCII value of lower case of "<<x<<" "<<int(x)<<", value in uppercase "<<int(y);
+    cout<<"ASCII value of lower case of "<<x<<" "<<int(x)<<", value in uppercase "<<int(y); //printing ascii value
     return 0;
 }
