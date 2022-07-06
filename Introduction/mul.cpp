05115d144f5f1d0883efa927e42e0de4669e36b1
Shubhranshu Bhardwaj <94945909+shubhranshu-bhardwaj@users.noreply.github.com>
2022-07-06 15:53:33 +0530

    C++Program to Multiply Two Numbers
    C++Program to Multiply Two Numbers

--- Introduction/mul.cpp
+++ Introduction/mul.cpp
@@ -1,9 +1,9 @@
 //C++Program to Multiply Two Numbers
 
-#include<iostream>
-using namespace std;
-int main(){
-    int x=5,y=6;
-    cout<<"Multiplication of "<<x<<" and "<<y<<" is:"<<x*y;
+#include<iostream> //header file
+using namespace std; //namespace
+int main(){ //main function
+    int x=5,y=6; //declaring variable
+    cout<<"Multiplication of "<<x<<" and "<<y<<" is:"<<x*y; //printing multiplication
     return 0;
 }
