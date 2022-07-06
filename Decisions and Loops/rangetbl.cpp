587a7f39563df85349c60b44905c27c27c1613c6
Shubhranshu Bhardwaj <94945909+shubhranshu-bhardwaj@users.noreply.github.com>
2022-07-06 17:02:21 +0530

    Display multiplication table up to a given range
    Display multiplication table up to a given range

--- Decisions and Loops/rangetbl.cpp
+++ Decisions and Loops/rangetbl.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main(){
     int x=5,range=15;
-    for (int i = 1; i <=range; i++)
+    for (int i = 1; i <=range; i++) //this loop will run till the condition is true i.e i<=range means i=15, this will multiply 5 from 1 to 15
     {
         cout<<x<<" x "<<i<<" = "<<x*i<<endl;
     }
