cf9fe15a6400393de7c124f03de881bf24a34584
Shubhranshu Bhardwaj <94945909+shubhranshu-bhardwaj@users.noreply.github.com>
2022-07-06 15:47:10 +0530

    Add two numbers and take input from user

--- Introduction/addtwouser.cpp
+++ Introduction/addtwouser.cpp
@@ -1,13 +1,13 @@
 //C++ program to add two number entered by user
 
-#include<iostream>
-using namespace std;
-int main(){
-    int x,y;
-    cout<<"Enter first no.:";
-    cin>>x;
+#include<iostream> //header file
+using namespace std; //namespace
+int main(){ //main function
+    int x,y; //declaring two variable
+    cout<<"Enter first no.:"; //display this on screen
+    cin>>x; //store the value entered by user
     cout<<"Enter second no.:";
     cin>>y;
-    cout<<"Addittion of your no. "<<x<<" and "<<y<<" is "<<x+y;
+    cout<<"Addittion of your no. "<<x<<" and "<<y<<" is "<<x+y; //printing the addition
     return 0;
 }
