6432cbf4a541bcf3354941f0ed79c01d39fc8626
Shubhranshu Bhardwaj <94945909+shubhranshu-bhardwaj@users.noreply.github.com>
2022-07-06 16:18:47 +0530

    swap two numbers take input from user
    C++ program to swap two numbers take input from user

--- Introduction/swap2user.cpp
+++ Introduction/swap2user.cpp
@@ -9,9 +9,9 @@ int main(){
     cout<<"Enter second no.:";
     cin>>y;
     cout<<"Before swapping first no. was:"<<x<<",second no. was:"<<y<<endl;
-    x=x+y;
-    y=x-y;
-    x=x-y; 
+    x=x+y; //let x=5,y=10, so x will be 15
+    y=x-y; //5
+    x=x-y; //10
     cout<<"After swapping first no. is:"<<x<<", second no. is:"<<y;
     return 0;
 }
