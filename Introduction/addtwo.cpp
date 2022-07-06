77009c82f10302a37415637f1bdd1a1cebf79b1c
Shubhranshu Bhardwaj <94945909+shubhranshu-bhardwaj@users.noreply.github.com>
2022-07-06 15:43:11 +0530

    Update addtwo.cpp
    Add two numbers

--- Introduction/addtwo.cpp
+++ Introduction/addtwo.cpp
@@ -1,11 +1,11 @@
 //C++ program to add two number
 
-#include<iostream>
-using namespace std;
+#include<iostream> //header file
+using namespace std; //namespace
 int main(){
-    int a,b;
-    a=5;
-    b=6;
-    cout<<"Addition of a and b is:"<<a+b; 
+    int a,b; //declaring variable
+    a=5; //assigning value
+    b=6; //assigning value
+    cout<<"Addition of a and b is:"<<a+b; //printing the sum
     return 0;
 }
