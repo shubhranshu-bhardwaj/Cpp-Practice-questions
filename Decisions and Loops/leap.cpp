1e0dc5be7ed01240ef81d552c14abe56cde504ab
Shubhranshu Bhardwaj <94945909+shubhranshu-bhardwaj@users.noreply.github.com>
2022-07-06 16:57:43 +0530

    C++ Program to Check Leap Year
    C++ Program to Check Leap Year

--- Decisions and Loops/leap.cpp
+++ Decisions and Loops/leap.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 int main(){
     int yr=2020;
-    if (yr%4==0 || yr%400==0)
+    if (yr%4==0 || yr%400==0) //if the year is totally divisible by 4 or 400 then it is called a leap year
     {
         cout<<"Leap year";
     }
