aa43fe9c81a05116c337090921bd5ed4637ad7a1
Shubhranshu Bhardwaj <94945909+shubhranshu-bhardwaj@users.noreply.github.com>
2022-07-06 16:47:52 +0530

    C++ Program to Find Factorial, using do-while loop and take input from user
    C++ Program to Find Factorial, using do-while loop and take input from user

--- Decisions and Loops/factdo.cpp
+++ Decisions and Loops/factdo.cpp
@@ -8,9 +8,9 @@ int main(){
     cin>>x;
     do
     {
-        fact*=i;
+        fact*=i; //this will run(multiply) till the condition is true (fact*=i=1*1=1, fact*=i=1*2=2, fact*=i=2*3=6, fact*=i=6*4=24, fact*=i=24*5=120)
         i++;
-    } while (i<=x);
+    } while (i<=x); //condition
     cout<<"Factorial of your no. "<<x<<" is:"<<fact;
     return 0;
 }
