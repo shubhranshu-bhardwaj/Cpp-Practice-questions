21c7c9468a7c623ecfbcd0ac815df8e213968c3c
Shubhranshu Bhardwaj <94945909+shubhranshu-bhardwaj@users.noreply.github.com>
2022-07-06 16:46:37 +0530

    C++ Program to Find Factorial
    C++ Program to Find Factorial

--- Decisions and Loops/fact.cpp
+++ Decisions and Loops/fact.cpp
@@ -3,15 +3,15 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int x=5,fact=1;
-    if (x<=0)
+    int x=5,fact=1; 
+    if (x<=0) //assigning condition if x is less or equal to 0 then if part print
     {
         cout<<"Enter num>0";
     }
     else
     {
-        for(int i=1;i<=x;i++)
-        fact*=i;
+        for(int i=1;i<=x;i++) //we are creating a loop for finding our factorial 
+        fact*=i; //this will run(multiply) till the condition is true (fact*=i=1*1=1, fact*=i=1*2=2, fact*=i=2*3=6, fact*=i=6*4=24, fact*=i=24*5=120)
     }
     cout<<"Factorial of your no "<<x<<" is:"<<fact;
     return 0;
