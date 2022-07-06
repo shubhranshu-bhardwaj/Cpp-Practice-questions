bb05ec3e9e488d0ff9ef970315a9a5cfce70dd2a
Shubhranshu Bhardwaj <94945909+shubhranshu-bhardwaj@users.noreply.github.com>
2022-07-06 19:55:59 +0530

    Add files via upload

---
+++ Flow Control/absolute.cpp
@@ -0,0 +1,12 @@
+// Find the absolute value of a number entered by the user
+#include <iostream>
+#include <cmath> //cmath is a math library of c++
+using namespace std;
+int main()
+{
+    float x;
+    cout << "Enter any number:";
+    cin >> x;
+    cout << "Absolute value is:" << abs(x); //we can print the absolute value by using abs(variable)
+    return 0;
+}
