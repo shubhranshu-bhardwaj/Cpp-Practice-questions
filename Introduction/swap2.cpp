83654b6d90b927b4a87262c1aaeccbf1921d89e0
Shubhranshu Bhardwaj <94945909+shubhranshu-bhardwaj@users.noreply.github.com>
2022-07-06 16:13:03 +0530

    C++ program to swap two numbers
    C++ program to swap two numbers

--- Introduction/swap2.cpp
+++ Introduction/swap2.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int main(){
     int x=5,y=10;
     cout<<"Before swapping x was:"<<x<<" y was:"<<y<<endl;
-    x=x+y;
-    y=x-y;
-    x=x-y;
+    x=x+y; //we will do some mathematics here, x=x(5)+y(10)=15
+    y=x-y; //y=x(15)-y(10)=5
+    x=x-y; //x=x(15)-y(5)=10
     cout<<"After swapping x is:"<<x<<" y is:"<<y;
     return 0;
 }
