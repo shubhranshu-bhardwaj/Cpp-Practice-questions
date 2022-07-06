7f4a6ef57d475d339604d2e08667155a424a572d
Shubhranshu Bhardwaj <94945909+shubhranshu-bhardwaj@users.noreply.github.com>
2022-07-06 17:16:10 +0530

    C++ Program to Calculate Sum of Natural Numbers
    C++ Program to Calculate Sum of Natural Numbers

--- Decisions and Loops/sum.cpp
+++ Decisions and Loops/sum.cpp
@@ -6,7 +6,7 @@ int main(){
     int x=13,sum=0;
     for (int i= 1; i <=x; i++)
     {
-        sum=sum+i;
+        sum=sum+i; //this will add 1 to 13(1+2+......+13) because our loop is starting from 1 and will end at 13
     }
     cout<<"Sum of your no. "<<x<<" is:"<<sum;
     return 0;
