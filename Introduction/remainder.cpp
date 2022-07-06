8b501bfdfc98ddb21e6c597a9af7938706a16cc0
Shubhranshu Bhardwaj <94945909+shubhranshu-bhardwaj@users.noreply.github.com>
2022-07-06 15:59:23 +0530

    C++ Program to Find Quotient and Remainder
    C++ Program to Find Quotient and Remainder

--- Introduction/remainder.cpp
+++ Introduction/remainder.cpp
@@ -1,12 +1,12 @@
 //C++ Program to Find Quotient and Remainder
 
-#include<iostream>
-using namespace std;
-int main(){
-    int x,y;
-    x=7;
-    y=3;
-    cout<<"Quotient:"<<x/y<<endl;
-    cout<<"Remainder:"<<x%y<<endl;
+#include<iostream> //header file
+using namespace std; //namespace
+int main(){ //main function
+    int x,y; //declaring variable
+    x=7; //assiging value
+    y=3; //assiging value
+    cout<<"Quotient:"<<x/y<<endl; //printing the quotient '/' use for finding quotient
+    cout<<"Remainder:"<<x%y<<endl; //printing the remainder '%' use for finding remainder
     return 0;
 }
