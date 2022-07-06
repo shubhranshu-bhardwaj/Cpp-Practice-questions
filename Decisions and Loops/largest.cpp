6ff86b9dfc7e03a31383f359c64eed73df94b80d
Shubhranshu Bhardwaj <94945909+shubhranshu-bhardwaj@users.noreply.github.com>
2022-07-06 16:53:05 +0530

    C++ Program to Find Largest Number Among Three Numbers
    C++ Program to Find Largest Number Among Three Numbers

--- Decisions and Loops/largest.cpp
+++ Decisions and Loops/largest.cpp
@@ -4,15 +4,15 @@
 using namespace std;
 int main(){
     int x=5,y=9,z=4;
-    if (x>y && x>z)
+    if (x>y && x>z) //first condition if this will true then if part printed
     {
         cout<<x<<" is largest";
     }
-    else if (y>x && y>z)
+    else if (y>x && y>z) //second condition if this will true then else if part printed
     {
         cout<<y<<" is largest";
     }
-    else
+    else //if both the condition not true then else part printed
     {
         cout<<z<<" is largest";
     }
