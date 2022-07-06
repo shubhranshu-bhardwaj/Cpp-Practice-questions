b194ddbbbdaa110e1278ce9d78ff572f132397b0
Shubhranshu Bhardwaj <94945909+shubhranshu-bhardwaj@users.noreply.github.com>
2022-07-06 16:21:49 +0530

    Swap Two Numbers take input from user
    C++ Program to Swap Two Numbers take input from user

--- Introduction/swapuser.cpp
+++ Introduction/swapuser.cpp
@@ -9,9 +9,9 @@ int main(){
     cout<<"Enter second no.:";
     cin>>y;
     cout<<"Before swapping first no. was:"<<x<<" second no. was:"<<y<<endl;
-    temp=x;
-    x=y;
-    y=temp;
+    temp=x; //temp is a temporary variable which store the value of x, let x=5
+    x=y; //now value of y will store in x, let y was 10 so now x will be 10
+    y=temp; //value of temp that is x=5 will store in y, so y will be 5
     cout<<"After swapping first no. is:"<<x<<" second no. is:"<<y<<endl;
     return 0;
 }
