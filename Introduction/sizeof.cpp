7cd5406e67ea898158de60d247d954fd6f9b5dff
Shubhranshu Bhardwaj <94945909+shubhranshu-bhardwaj@users.noreply.github.com>
2022-07-06 16:03:23 +0530

    Find Size of int, float, double and char in Your System
    C++ Program to Find Size of int, float, double and char in Your System

--- Introduction/sizeof.cpp
+++ Introduction/sizeof.cpp
@@ -3,9 +3,9 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"Size of int data types:"<<sizeof(int)<<" byte"<<endl;;
-    cout<<"Size of float data types:"<<sizeof(float)<<" byte"<<endl;;
-    cout<<"Size of double data types:"<<sizeof(double)<<" byte"<<endl;;
+    cout<<"Size of int data types:"<<sizeof(int)<<" byte"<<endl; //we use sizeof keyword for finding the size of any data types
+    cout<<"Size of float data types:"<<sizeof(float)<<" byte"<<endl;
+    cout<<"Size of double data types:"<<sizeof(double)<<" byte"<<endl;
     cout<<"Size of char data types:"<<sizeof(char)<<" byte";
     return 0;
 }
