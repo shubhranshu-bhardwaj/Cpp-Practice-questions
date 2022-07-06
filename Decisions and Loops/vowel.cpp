13be3182466cf4b4f6d56220efff37844e632993
Shubhranshu Bhardwaj <94945909+shubhranshu-bhardwaj@users.noreply.github.com>
2022-07-06 17:38:33 +0530

    C++ Program to Check Whether a character is Vowel or Consonant.
    C++ Program to Check Whether a character is Vowel or Consonant.

--- Decisions and Loops/vowel.cpp
+++ Decisions and Loops/vowel.cpp
@@ -5,15 +5,15 @@ using namespace std;
 int main(){
     char x='a';
     bool lowercase,uppercase;
-    lowercase=(x=='a'|| x=='e'||x=='i'||x=='o'||x=='u');
-    uppercase=(x=='A'|| x=='E'||x=='I'||x=='O'||x=='U');
-    if (lowercase || uppercase)
+    lowercase=(x=='a'|| x=='e'||x=='i'||x=='o'||x=='u'); //is character in lowercase
+    uppercase=(x=='A'|| x=='E'||x=='I'||x=='O'||x=='U'); //is character in upppercase
+    if (lowercase || uppercase) //if character is in lowercase or in upper case then if part will print
     {
         cout<<"Vowel";
     }
-    else
+    else //if above condition not true then else part will print
     {
-        cout<<"Consonant";
+        cout<<"Consonant"; 
     }
     return 0;
     
