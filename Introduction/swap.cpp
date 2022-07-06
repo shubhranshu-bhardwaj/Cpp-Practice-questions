4cac3e6db9dff17383ccff1f7ca8ef2c610b5222
Shubhranshu Bhardwaj <94945909+shubhranshu-bhardwaj@users.noreply.github.com>
2022-07-06 16:06:23 +0530

    C++ Program to Swap Two Numbers
    C++ Program to Swap Two Numbers using temp variable

--- Introduction/swap.cpp
+++ Introduction/swap.cpp
@@ -3,11 +3,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a=5,b=10, temp;
+    int a=5,b=10, temp; 
     cout<<"Before swapping a is:"<<a<<" b is:"<<b<<endl;
-    temp=a;
-    a=b; 
-    b=temp;
+    temp=a; //temp is a temporary variable which store the value of a
+    a=b; //now we store the value of b into a 
+    b=temp; //now we store the value of tem into b, which the value of a
     cout<<"After swapping a is:"<<a<<" b is:"<<b<<endl;
     return 0;
 }
