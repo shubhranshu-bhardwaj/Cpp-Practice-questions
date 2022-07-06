54fb45533ec66f4b0fa885541f28c6eaccc686c6
Shubhranshu Bhardwaj <94945909+shubhranshu-bhardwaj@users.noreply.github.com>
2022-07-06 17:27:28 +0530

    C++ program to print table of a number
    C++ program to print table of a number

--- Decisions and Loops/table.cpp
+++ Decisions and Loops/table.cpp
@@ -3,13 +3,10 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a=2, table;
+    int a=2;
     for (int i = 1; i <=10; i++)
     {
-        //table=a*i;
-        //cout<<table<<endl;
-        //cout<<a*i<<endl;
-        cout<<a<<" x "<<i<<" = "<<a*i<<endl;
+        cout<<a<<" a "<<i<<" = "<<a*i<<endl; //this will print from 1 to 10 (2*1,2*2,....2*10) because our loop starting from 1 and will end at 10
     }
     
     return 0;
