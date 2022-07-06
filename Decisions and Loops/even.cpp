af7312676aaa0706540df1abe306861bac3df468
Shubhranshu Bhardwaj <94945909+shubhranshu-bhardwaj@users.noreply.github.com>
2022-07-06 16:37:42 +0530

    C++ Program to Check Whether Number is Even or Odd
    C++ Program to Check Whether Number is Even or Odd

--- Decisions and Loops/even.cpp
+++ Decisions and Loops/even.cpp
@@ -4,12 +4,12 @@
 using namespace std;
 int main(){
     int x=5;
-    if (x%2==0)
+    if (x%2==0) //if any number totally divisible by 2 then it's an even no
     {
         cout<<"It's an even no.";
     }
     else{
-        cout<<"It's an odd no.";
+        cout<<"It's an odd no."; //if the if part will not true then else part will be printed
     }
     return 0;
     
