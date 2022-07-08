ec38afeb98ce06f25ed7dd5e44952ec2d56073d3
Shubhranshu Bhardwaj <94945909+shubhranshu-bhardwaj@users.noreply.github.com>
2022-07-08 11:58:21 +0530

    Write a program to find the length of string.
    Write a program to find the length of string.

---
+++ String/length.cpp
@@ -0,0 +1,10 @@
+// Write a program to find the length of string.
+#include <iostream>
+#include <string> //if we add string in our program then we have to add this library
+using namespace std;
+int main()
+{
+    string txt = "Adarsh";
+    cout << "Length of string is:" << txt.length(); // for calculating the length of string we use string name.length()
+    return 0;
+}
