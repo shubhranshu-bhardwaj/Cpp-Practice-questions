64e1ec329f50dff4f9bb2ab35e56e6746a634914
Shubhranshu Bhardwaj <94945909+shubhranshu-bhardwaj@users.noreply.github.com>
2022-07-08 12:40:08 +0530

    C++ program to print string
    C++ program to print string

---
+++ String/store.cpp
@@ -0,0 +1,10 @@
+// C++ program to declare and print string
+#include <iostream>
+#include <string> //if we add string in our program we have to add this library
+using namespace std;
+int main()
+{
+    string name = "Adarsh"; // declaring string
+    cout << "Your name is:" << name;
+    return 0;
+}
