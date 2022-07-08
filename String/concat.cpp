a8022c7a7094006111628f55578c0aa9fcf1042a
Shubhranshu Bhardwaj <94945909+shubhranshu-bhardwaj@users.noreply.github.com>
2022-07-08 11:42:04 +0530

    C++ program to concate string
    C++ program to concate string

---
+++ String/concat.cpp
@@ -0,0 +1,12 @@
+// C++ program to concate string
+#include <iostream>
+#include <string> //if we add string in our program then we  have to add this library
+using namespace std;
+int main()
+{
+    string firstname, lastname;
+    firstname = "Shubhranshu";
+    lastname = "Bhardwaj";
+    cout << firstname + " " + lastname; // concate means adding two string
+    return 0;
+}
