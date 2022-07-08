2748104e93c8a77939e368c2d41a4f296e7c414d
Shubhranshu Bhardwaj <94945909+shubhranshu-bhardwaj@users.noreply.github.com>
2022-07-08 11:14:27 +0530

    C++ program to access any character of string
    C++ program to access any character of string

---
+++ String/access.cpp
@@ -0,0 +1,9 @@
+//C++ program to access any character of string
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string txt="Adarsh";
+    cout<<"Third character of your word is:"<<txt[2];
+    return 0;
+}
